4972ab5730efbbdd85cbae690d6708b6e6dd0367
lev.kp <levente.kataipal@pm.me>
2024-04-03 22:55:39 +0200

    Added config.ini.example, updated .gitignore

--- rssify.py
+++ rssify.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 from pytz import timezone
 
 config = configparser.ConfigParser()
-config.read('config.ini')
+config.read('config.ini.example')
 
 for section in config.sections():
     s = dict(config.items(section))
